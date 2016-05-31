3f094fbc526593deffa833aa53639ff57ca555ca
Nikita <nikita.kamyshenko@mail.ru>
2016-05-31 21:27:30 +0300

    setting log

--- src/main/java/com/GameEngine/logic/dynamicValues/DynamicParameterMap.java
+++ src/main/java/com/GameEngine/logic/dynamicValues/DynamicParameterMap.java
@@ -19,27 +19,27 @@ public class DynamicParameterMap implements DynamicParameter {
 
     @Override
     public void putParameter(String name, String value) {
-        logger.debug("New param Name:" + name + " value:" + value);
+        logger.debug("putParameter Name:" + name + " value:" + value);
         mapParameter.put(name, value);
     }
 
     @Override
     public String getParameter(String name) {
         String value = mapParameter.get(name);
-        logger.debug("Get param Name:" + name + " value:" + value);
+        logger.debug("getParameter Name:" + name + " value:" + value);
         return value;
     }
 
     @Override
     public void putParameterInt(String name, int value) throws NumberFormatException {
-        logger.debug("New paramInt Name:" + name + " value:" + value);
+        logger.debug("putParameterInt Name:" + name + " value:" + value);
         mapParameter.put(name, value + "");
     }
 
     @Override
     public int getParameterInt(String name) throws NumberFormatException {
         int value = Integer.parseInt(mapParameter.get(name));
-        logger.debug("Get paramInt Name:" + name + " value:" + value);
+        logger.debug("getParameterInt Name:" + name + " value:" + value);
         return value;
     }
 
